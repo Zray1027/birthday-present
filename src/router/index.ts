480f25785e854fea1322fb9f5855eb37d0270350
zray <zray1027@outlook.com>
2025-11-19 19:06:54 +0800

    refactor(router): 修正视图组件导入路径
    更新路由配置中的组件导入路径，从'@'别名改为相对路径，同时ts构建信息文件自动更新了包含的文件列表

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '@/views/Home/Home.vue'
-import Like from '@/views/Like/Like.vue'
+import Home from '../views/Home/Home.vue'
+import Like from '../views/Like/Like.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
